2706eca3c7e576f6b06c91944e572cb488574969
Imon Bhuiya <imon.cse15@gmail.com>
2025-08-19 23:33:46 +0200

    MongoDB container create

--- src/test/java/com/banking/microservices/product/ProductApplicationTests.java
+++ src/test/java/com/banking/microservices/product/ProductApplicationTests.java
@@ -1,15 +1,36 @@
 package com.banking.microservices.product;
 
+import com.mongodb.client.MongoClient;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApplicationTests {
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+	static {
+		mongoDBContainer.start();
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
 	}
 
 }
